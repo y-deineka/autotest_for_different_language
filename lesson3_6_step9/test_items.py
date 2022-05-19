6a93e6e74f8e9ccc24af3662c3bd5720f36a8988
y-deineka <deinekawow@gmail.com>
2022-05-19 16:09:39 +0300

    rename function name

--- lesson3_6_step9/test_items.py
+++ lesson3_6_step9/test_items.py
@@ -1,6 +1,6 @@
 import time
 
-def test_guest_should_see_buy_button(browser):
+def test_should_see_buy_button(browser):
 
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     #time.sleep(30)                                   #Уберите знак "#" в начале строки, чтобы активировать задержку
