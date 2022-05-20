349f988afd594060728496e9f959eccb45a784ab
y-deineka <deinekawow@gmail.com>
2022-05-20 11:27:25 +0300

    new asserts on login page

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from pages.base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        assert 'login' in current_url, f'there is no such word {current_url} in url'
+
+    def should_be_login_form(self):
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), 'Login form is not presented'
+
+    def should_be_register_form(self):
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), 'Register form is not presented'
