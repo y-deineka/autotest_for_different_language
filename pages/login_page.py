847e34afc8a3e6f3e5b3a06eb2041130b0d19bb0
y-deineka <deinekawow@gmail.com>
2022-05-21 11:33:14 +0300

    ...

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,8 +10,9 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
+        word = 'login'
         current_url = self.browser.current_url
-        assert 'login' in current_url, f'there is no such word {current_url} in url'
+        assert word in current_url, f'there is no such word {word} in {current_url}'
 
     def should_be_login_form(self):
         assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), 'Login form is not presented'
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,14 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        browser = self.browser
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
     def should_be_login_link(self):
         assert self.browser.find_element(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
